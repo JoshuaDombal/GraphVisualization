0a826f80cb2dfaa0f9e8033ad963fbefab1fa8d4
dombalj <dombalj@wwu.edu>
2018-10-24 20:50:15 -0700

    Started

---
+++ CreateGraph.py
@@ -0,0 +1,13 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+from numpy import genfromtxt
+import numpy as np
+
+
+
+mydata = genfromtxt('matrix.csv', delimiter=',')
+print(mydata)
+print(type(mydata))
+
+
